cde074fce9043c7d991616d482e46d9baabf3594
810Loaf <137736680+810Loaf@users.noreply.github.com>
2024-11-17 02:54:28 -0500

    Update flash.js

--- flash.js
+++ flash.js
@@ -22,7 +22,6 @@ function showSlides(n) {
   }
 
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
 
 function updateSlide(n, slideContent, slideContent2) {
